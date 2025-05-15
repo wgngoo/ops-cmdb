d40fa544d41615d0ccd6e892b78b5a3ac55c728f
shen <wn@tcl.com>
2025-05-15 10:41:01 +0800

    first commit

--- pkg/cmdb_sdk_go/utils/string.go
+++ pkg/cmdb_sdk_go/utils/string.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"encoding/json"
+	"log"
+)
+
 // String 将字符串转换为指针
 func String(s string) *string {
 	return &s
@@ -11,4 +16,14 @@ func StringValue(a *string) string {
 		return ""
 	}
 	return *a
+}
+
+func StringFormat(queryStr string) (map[string]interface{}, error) {
+	var result map[string]interface{}
+	var err error
+	if err = json.Unmarshal([]byte(queryStr), &result); err != nil {
+		log.Fatalf("解析查询条件失败: %v", err)
+		return result, err
+	}
+	return result, nil
 }
